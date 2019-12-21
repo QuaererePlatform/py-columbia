7f6016fb642a7de57897b9554a0e25d899dcc66c
ravenoak <ravenoak@virtualxistenz.com>
2019-12-21 13:49:31 -0800

    Small clean-up in tasks

--- columbia/tasks/common_crawl.py
+++ columbia/tasks/common_crawl.py
@@ -5,29 +5,33 @@ __all__ = [
     'update_web_site_cc_data',
 ]
 
-import gzip
-import io
 import json
 
 from arango import DocumentInsertError
 from celery.utils.log import get_task_logger
+import requests
+
 from columbia_common.schemas.api_v1 import (
     CCDataSchema,
     CCScansSchema,
     CCIndexesSchema)
-import requests
 
 from columbia.config import common as config
 from columbia.models.api_v1.common_crawl import (
     CCDataModel,
     CCScansModel,
     CCIndexesModel)
-from .app import app, CCScanTask, ColumbiaTask
+from .app import (
+    app,
+    CCScanTask,
+    ColumbiaTask)
+from .util import (
+    get_data_from_cc,
+    get_web_site_from_willamette,
+    get_web_page_body_from_cc,
+    web_page_exists)
 
 LOGGER = get_task_logger(__name__)
-
-
-
 
 
 @app.task(base=CCScanTask, bind=True)
@@ -46,33 +50,10 @@ def get_cc_data(self, web_site_key, cc_index_key):
 
     # Until client code is more stable, use requests directly
     # web_site = self.willamette.v1.web_site.get(web_site_key)
-    resp = requests.get(config.WILLAMETTE_URL + f'v1/web-site/{web_site_key}')
-    if not resp.ok:
-        LOGGER.error(f'Could not retrieve web_site data; '
-                     f'_key={web_site_key}, reason: {resp.json()}')
-        raise Exception  # FIXME
-    web_site = resp.json()
+    web_site = get_web_site_from_willamette(web_site_key)
     cc_index = self.db_conn.query(CCIndexesModel).by_key(cc_index_key)
 
-    index_url = cc_index.cdx_api
-    web_site_url = web_site['url']
-
-    params = {'url': f'{web_site_url}/*',
-              'filter': '=status:200',
-              'output': 'json'}
-    LOGGER.info('Searching for website using CommonCrawl index'
-                f' {cc_index_key}',
-                extra={'cc_index_id': cc_index_key,
-                       'index_url': index_url,
-                       'web_site_url': web_site_url, })
-    req = requests.get(index_url, params)
-    if req.status_code != 200:
-        LOGGER.error(f'Error searching {cc_index_key}',
-                     stack_info=True,
-                     extra={'params': params,
-                            'index_url': index_url})
-        raise Exception  # FIXME
-    records = req.text.splitlines()
+    records = get_data_from_cc(web_site['url'], cc_index_key, cc_index.cdx_api)
     LOGGER.info(f'Found {len(records)} records')
     for record in records:
         unmarshal = CCDataSchema().load(json.loads(record))
@@ -94,42 +75,18 @@ def get_cc_data(self, web_site_key, cc_index_key):
 
 @app.task(base=ColumbiaTask, bind=True)
 def download_web_page_data(self, cc_data_key, web_site_key):
+    """
+
+    :param self:
+    :param cc_data_key:
+    :param web_site_key:
+    :return:
+    """
     cc_data = self.db_conn.query(CCScansModel).by_key(cc_data_key)
-    url = config.CC_DATA_URL_PREFIX + cc_data.filename
-    file_end = cc_data.offset + cc_data.length - 1
-    headers = {'Range': f'bytes={cc_data.offset}-{file_end}'}
-    LOGGER.info()
-    resp = requests.get(url, headers=headers)
-    if not resp.ok:
-        LOGGER.critical(f'Could not get data for web page: {cc_data}; '
-                        f'{resp.status_code}, {resp.reason}')
-        raise Exception  # FIXME
-    web_page_data = gzip.GzipFile(
-        fileobj=io.BytesIO(resp.content)).read().decode()
-    _, _, html = web_page_data.strip().split('\r\n\r\n', 2)
-    url = config.WILLAMETTE_URL + 'v1/web-page/find/'
-    params = {
-        'conditions': [
-            'url==@url',
-            'source_accounting.datetime_acquired==@date',
-            'source_accounting.data_origin==@origin'
-        ],
-        'variables': {
-            'url': cc_data.url,
-            'date': cc_data.timestamp,
-            'origin': 'common_crawl'
-        },
-    }
-    resp = requests.get(url, params=params)
-    if not resp.ok:
-        LOGGER.critical(f'Could not query willamette for web page: {params}; '
-                        f'{resp.status_code}, {resp.reason}')
-        raise Exception  # FIXME
-    q_data = resp.json()
-    if len(q_data['result']) == 0:
+    if not web_page_exists(cc_data):
         url = config.WILLAMETTE_URL + 'v1/web-page/'
         web_page_data = {
-            'text': html,
+            'text': get_web_page_body_from_cc(cc_data),
             'url': cc_data.url,
             'web_site_key': web_site_key,
             'source_accounting': {
@@ -179,8 +136,12 @@ def update_web_site_cc_data(self, web_site_key):
                 LOGGER.warning(err)
 
 
-@app.task(base=CCScanTask, bind=True)
-def update_all_web_site_cc_data(self):
+@app.task(base=CCScanTask)
+def update_all_web_site_cc_data():
+    """
+
+    :return:
+    """
     scan_url = config.COLUMBIA_URL_PREFIX + '/api/v1/cc-scans/scan-web-site/'
     web_sites_url = config.WILLAMETTE_URL + 'v1/web-sites/'
     resp = requests.get(web_sites_url)
